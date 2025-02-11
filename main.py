d0306dacfea3d9918df48e1f59043901d410a715
svelozs <163176096+svelozs@users.noreply.github.com>
2025-02-11 12:37:23 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from fastapi.responses import Response
 
 app = FastAPI()
 
 @app.post("/webhook", response_class=Response)
-async def webhook(request: Request):
-    # Verifica que Twilio esté enviando la solicitud
-    print("Recibiendo solicitud de Twilio:", await request.json())
-    
+async def webhook():
     xml_content = """<?xml version="1.0" encoding="UTF-8"?>
     <Response>
-        <Say language="es-ES" voice="Polly.Latino-ES">Hola, gracias por llamar. ¿En qué puedo ayudarte?</Say>
+        <Say>Hola, gracias por llamar. ¿En qué puedo ayudarte?</Say>
     </Response>"""
-    
     return Response(content=xml_content, media_type="application/xml")

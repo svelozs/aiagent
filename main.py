de802e98c439ac58a447094a476e324e0d0493ea
svelozs <163176096+svelozs@users.noreply.github.com>
2025-02-11 12:18:54 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
-from fastapi.responses import Response  # Cambié XMLResponse por Response
+from fastapi.responses import Response
 
 app = FastAPI()
 
-@app.get("/", response_class=Response)
-async def read_root():
+@app.post("/webhook", response_class=Response)
+async def webhook():
     xml_content = """<?xml version="1.0" encoding="UTF-8"?>
-    <message>Hello, World!</message>"""
+    <Response>
+        <Say>Hola, gracias por llamar. ¿En qué puedo ayudarte?</Say>
+    </Response>"""
     return Response(content=xml_content, media_type="application/xml")

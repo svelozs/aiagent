f681d4d6d3670f7be619c9a1138fbf91e411c65a
svelozs <163176096+svelozs@users.noreply.github.com>
2025-02-14 14:47:30 -0600

    Cambio de voz

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ app = FastAPI()
 async def webhook():
     xml_content = """<?xml version="1.0" encoding="UTF-8"?>
     <Response>
-        <Say voice="Polly.Miguel" language="es-ES">Hola, mi nombre es Sonia Veloz y estoy aquí para atenderte. ¿En que puedo ayudarte?</Say>
+        <Say voice="Polly.Lupe" language="es-MX">Hola, mi nombre es Sonia Veloz y estoy aquí para atenderte. ¿En qué puedo ayudarte?</Say>
     </Response>"""
     return Response(content=xml_content, media_type="application/xml")

37a0ae53b10657b80878b1872c56d36c83811b70
svelozs <163176096+svelozs@users.noreply.github.com>
2025-02-17 10:01:23 -0600

    Security update

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ import os
 from google.cloud import speech_v1p1beta1 as speech
 from google.protobuf.json_format import MessageToDict
 
-# Configurar la API Key de Google
-os.environ["GOOGLE_API_KEY"] = "AIzaSyAy9IV7l1Xj6_VW84qzX0CeYrv7A6A_LF0"
-
 app = FastAPI()
 
 @app.post("/webhook", response_class=Response)
